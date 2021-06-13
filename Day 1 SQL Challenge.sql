66a96251c09b69d483061ee7564ba4c8275d9125
sandeeptanjore <82674484+sandeeptanjore@users.noreply.github.com>
2021-06-13 18:16:08 -0400

    2nd day challenge
    This script contains a query posed for day 2 challenge

---
+++ Day 1 SQL Challenge.sql
@@ -0,0 +1,21 @@
+/* SQL Prep  Daily SQL Challenge #1 */
+
+/*
+Show the customer name for all customers who have ever made a payment over $100,000.
+*/
+
+SELECT DISTINCT C.customername
+FROM   customers C, payments P
+WHERE  C.customernumber = P.customernumber
+AND    p.amount > 100000;
+
+--Do this with EXISTS Operator
+SELECT C.customername       
+FROM customers C 
+WHERE EXISTS (SELECT 'X'
+              FROM payments P
+              WHERE C.customernumber = P.customernumber 
+              AND P.amount > 100000
+              );
+
+  
---
+++ Day 2 SQL Challenge.sql
@@ -0,0 +1,13 @@
+/* SQL Prep  Daily SQL Challenge #2 */
+
+/*
+Show the product code, product name, and quantity in stock of all products that have a purchase
+price greater than the average.
+*/      
+
+SELECT productcode,productname,quantityinstock,msrp
+FROM products
+WHERE msrp >(SELECT ROUND(AVG(msrp))
+                 FROM products
+                 )
+ORDER BY msrp;         
