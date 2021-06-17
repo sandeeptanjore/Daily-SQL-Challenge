403e62a1dfbc38d4ad76da51c6d6d6aefa45831c
sandeeptanjore <82674484+sandeeptanjore@users.noreply.github.com>
2021-06-17 15:48:55 -0400

    Day 4 SQL Challenge
    Script for Day 4 challenge

---
+++ Day 4 SQL Challenge.sql
@@ -0,0 +1,13 @@
+/*                              SQL Prep  Daily SQL Challenge #4 */
+
+/*
+Show the employee first name, last name, and job title for all employees with the job title of Sales Rep.
+
+*/
+
+SELECT firstname,lastname,jobtitle
+FROM employees 
+WHERE jobtitle ='Sales Rep';
+ 
+ 
+ 
